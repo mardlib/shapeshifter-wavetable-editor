1ee8efc755d2f77404ad3fe35025bc7b42cda11c
Martin Sovinz <martin.dibek@icloud.com>
2026-08-15 07:54:12 +0200

    Deploy offline PWA

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = "waveport-43b19e6b749d";
+const PRECACHE_URLS = [
+  "./",
+  "./assets/index-aZUmJYWC.js",
+  "./assets/index-k2on0yWN.css",
+  "./bridges/spiOverJtag_ep4ce2217.rbf",
+  "./favicon.svg",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./index.html",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((names) => Promise.all(names.filter((name) => name.startsWith("waveport-") && name !== CACHE_NAME).map((name) => caches.delete(name))))
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) return;
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          void caches.open(CACHE_NAME).then((cache) => cache.put("./", copy));
+          return response;
+        })
+        .catch(() => caches.match("./").then((response) => response || caches.match("./index.html"))),
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      if (response.ok) {
+        const copy = response.clone();
+        void caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      }
+      return response;
+    })),
+  );
+});
